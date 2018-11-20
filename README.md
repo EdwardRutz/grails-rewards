8d42d0ddec473062b45b21cdbf7ff591d5b408fe
edwardrutz <96d7cf74@opayq.com>
2018-11-19 20:12:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Rewards App
+
+> A customer rewards app
+> Work in Progress
+
+
+
+
+
+
+
+
+
+## Sources
+
+- [Mastering Grails](https://www.udemy.com/mastering-grails/learn/v4/t/lecture/1165654?start=15)
+
---
+++ grails-app/controllers/rewards/InventoryController.groovy
@@ -0,0 +1,12 @@
+package rewards
+
+class InventoryController {
+
+    def index() {
+        render "Here is a list of everything."
+    }
+
+    def remove() {
+        render "An item was removed."
+    }
+}
